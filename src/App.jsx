3f257497c24499ea4a542fddaaada1bd0bf5c93c
firdavsorginal <firdavstudimamatov@gmail.com>
2024-11-14 17:54:04 +0300

    gjf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
-import Firdavs from "./components/Firdavs/Firdavs";
+
 function App() {
   return (
     <>
-    <Firdavs />
+
       <Navbar />
       <Banner />
     </>
