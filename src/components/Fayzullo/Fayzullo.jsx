35abb537627b9e0a5f351f52fd06c21e146e2431
Creeper <anvarovfayzullo61@gmail.com>
2024-11-14 17:44:49 +0500

    TeamWork

---
+++ src/components/Fayzullo/Fayzullo.jsx
@@ -0,0 +1,30 @@
+import './Fayzullo.css'
+
+const Fayzullo = () => {
+    let img = 'https://cdn-icons-png.flaticon.com/512/8421/8421969.png'
+    return (
+        <>
+            <div className="container">
+                <section>
+                    <img src={img} alt="" />
+                    <div className="text_card">
+                        <h1>Bark Bond</h1>
+                        <h2>Hayotbek</h2>
+                        <ul>
+                            <li>Mohista</li>
+                            <li>Alixon</li>
+                            <li>MuhammadAli</li>
+                            <li>Murodjon</li>
+                            <li>Behruzbek</li>
+                            <li>Temurbek</li>
+                            <li>Firdavs</li>
+                            <li>Fayzullo</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
+
+export default Fayzullo
