2fa1f9e60c81374960302d89b9556f019be41e8e
firdavsorginal <firdavstudimamatov@gmail.com>
2024-11-14 17:41:12 +0300

    gjf

---
+++ src/components/Firdavs/Firdavs.jsx
@@ -0,0 +1,26 @@
+import "./Firdavs.css";
+
+const Firdavs = () => {
+  return (
+    <div className="con">
+      <div className="left11">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6dv7tupCmiqiqpmjW0G1aAxDqpUh0HMPNg&s"
+          alt="Supra image"
+        />
+      </div>
+      <div className="right11">
+        <h1>Supra: Dictionary Definition and Usage</h1>
+        <p>
+          The term supra is a Latin word meaning "above" or "beyond" and is
+          primarily used in formal, legal, or academic contexts. It serves as a
+          reference tool to direct readers to previously mentioned content,
+          helping maintain the flow of information without unnecessary
+          repetition. It can be employed both as an adjective and an adverb.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Firdavs;
