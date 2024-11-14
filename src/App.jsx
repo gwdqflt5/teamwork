12667ca37d15320c8697629020b2f07127ffe018
BehruzWeb <behruz33365@gmail.com>
2024-11-14 17:35:22 +0500

    Behruzbek

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Banner from "./components/Banner/Banner";
+import Behruzbek from "./components/Behruzbek/Behruzbek"
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar />
       <Banner />
+      <Behruzbek/>
     </>
   );
 }
---
+++ src/components/Behruzbek/Behruzbek.jsx
@@ -0,0 +1,12 @@
+import "./Behruzbek.css"
+
+const Behruzbek = () => {
+ return (
+  <div className='card'>
+   <img src={"https://images.pexels.com/photos/1480690/pexels-photo-1480690.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} alt="" />
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, omnis commodi! Neque architecto vero ea</p>
+  </div>
+ )
+}
+
+export default Behruzbek;
