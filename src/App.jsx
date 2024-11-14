7595a0e5e33635b9fed1359b47d06a1073bf5af4
gwdqflt5 <you@example.com>
2024-11-14 18:03:13 +0500

    team

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import "./App.css";
 import Alixon from "./components/Alixon/Alixon";
 import Banner from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
-
+import Behruzbek from "./components/Behruzbek/Behruzbek";
+import Murodjon from "./components/Murodjon/Murodjon";
+import Fayzullo from "./components/Fayzullo/Fayzullo";
 
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
-<<<<<<< HEAD
       <Alixon />
-=======
-
->>>>>>> origin/Murodjon
+      <Behruzbek />
+      <Murodjon />
+      <Fayzullo />
     </>
   );
 }
