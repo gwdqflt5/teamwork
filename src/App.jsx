2b0d8c356d305c97aae8f22cad51037eb8fe6246
firdavsorginal <firdavstudimamatov@gmail.com>
2024-11-16 16:59:18 +0300

    gjf

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
 
+
 function App() {
   return (
     <>
-
+  
       <Navbar />
       <Banner />
     </>
--- src/components/Firdavs/Firdavs.jsx
+++ src/components/Firdavs/Firdavs.jsx
@@ -5,19 +5,16 @@ const Firdavs = () => {
     <div className="con">
       <div className="left11">
         <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ6dv7tupCmiqiqpmjW0G1aAxDqpUh0HMPNg&s"
+          src="/Group 48095505.png"
           alt="Supra image"
         />
       </div>
       <div className="right11">
-        <h1>Supra: Dictionary Definition and Usage</h1>
+        <h1 className='h1'>What is Metaverse</h1>
         <p>
-          The term supra is a Latin word meaning "above" or "beyond" and is
-          primarily used in formal, legal, or academic contexts. It serves as a
-          reference tool to direct readers to previously mentioned content,
-          helping maintain the flow of information without unnecessary
-          repetition. It can be employed both as an adjective and an adverb.
+        Adipiscing at in tellus integer. Pellentesque massa placerat duis ultricies lacus. Nisi porta lorem mollis aliquam ut porttitor leo. Venenatis cras..
         </p>
+        <button>Know More</button>
       </div>
     </div>
   );
